29859f06f171f8b200c106e2074973c7d2548e3d
irismelendez <iris.13melendez@gmail.com>
2025-10-14 09:19:39 -0500

    I created a Home Screen System.out.print.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,42 @@
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
+
+        System.out.println("--------------------");
+        System.out.println("   Home Screen  ");
+        System.out.println("---------------------");
+        System.out.println("1).Add Deposit");
+        System.out.println("2).Make a payment(Debit)");
+        System.out.println("3).Ledger");
+        System.out.println("4).Exit");
+        System.out.println("---------------------");
+
+
+
+
+
+
+
+
+        //declared my variables
+
+
+        //display menu
+
+
+        //get and process user choices
+
+
+        //deposit()
+
 
     }
 
